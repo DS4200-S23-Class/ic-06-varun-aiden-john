8ab4ebc63d6e1dc7b1ab9a4071769d4eebb7ee3c
John McCarthy <johnmccarthy@Johns-MacBook-Pro.local>
2023-02-13 15:22:40 -0500

    ic-06 fin

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeLineColor() {
+
+    document.getElementById("line").style.stroke = "red"
+
+    
+
+}
+
+document.getElementById("lineButton")
+.addEventListener("click", changeLineColor)
